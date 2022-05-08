73a3d899d5664b5cad272168da91e2543c96196b
natsumi <aka.723n@gmail.com>
2022-05-08 15:40:52 +0900

    feat(SignUp): add useRef

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 export const SignUp = () => {
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const emailPassword = useRef<HTMLInputElement | null>(null);
   const handleSubmit = (event:
     React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('登録');
+    console.log(emailRef.current?.value, emailPassword.current?.value);
   };
+
+
 
   return (
     <div>
@@ -13,11 +17,11 @@ export const SignUp = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label>メールアドレス</label>
-          <input name="email" type="email" placeholder="email" />
+          <input name="email" type="email" placeholder="email" ref={emailRef}/>
         </div>
         <div>
           <label>パスワード</label>
-          <input name="password" type="password" />
+          <input name="password" type="password" ref={emailPassword}/>
         </div>
         <div>
           <button>登録</button>
