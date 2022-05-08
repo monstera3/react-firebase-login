1c262d48be302ea6bf0cc8a0858db26d854ef67e
natsumi <aka.723n@gmail.com>
2022-05-08 18:08:55 +0900

    feat: implement ログアウト

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+import { getAuth, signOut } from 'firebase/auth';
+
 export const Home = () => {
-  return <h1>ログイン画面</h1>;
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      console.log('sign out successful')
+    }).catch((error) => {
+      console.log(error);
+    });
+    navigate("/login", { replace: true })
+  }
+  return (
+    <div>
+      <h1>ホームページ</h1>
+      <button onClick={handleLogout}>ログアウト</button>
+    </div>
+  );
 }
